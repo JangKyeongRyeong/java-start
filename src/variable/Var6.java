3ebfd06a30f1673d1313b0cbb27d15e554bd3919
notyung@naver.com <notyung@naver.com>
2024-04-07 21:44:45 +0900

    java-변수

---
+++ src/variable/Var6.java
@@ -0,0 +1,9 @@
+package variable;
+
+public class Var6 {
+
+    public static void main(String[] args) {
+        int a;
+        //System.out.println(a); //지역변수는 초기화 해야된다.
+    }
+}
---
+++ src/variable/Var7.java
@@ -0,0 +1,24 @@
+package variable;
+
+public class Var7 {
+
+    public static void main(String[] args) {
+        /**
+         * 리터럴 :변수의값은 변할 수 있지만 리터럴은 개발자가 직접 입력한 고정된 값이다.
+         *        따라서 리터럴 자체는 변하지 않는다.
+         */
+
+        //변수 타입
+        int a = 100;             //정수
+        double b = 10.5;         //실수
+        boolean c = true;        //불리언 true, false 입력 가능
+        char d = 'A';            //문자 하나
+        String e = "Hello Java"; //문자열, 문자열을 다루기 위한 특별한 타입
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
